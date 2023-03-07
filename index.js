8b715dbed2f2ad9ae1154daa4aa8b0f192195882
AlejandroForeroG <alejandro.forerog@gmail.com>
2023-03-07 20:43:14 +0000

    Changues to the server doesn't run because of an error in socket io module

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-const path= require ('path')
 const express = require('express'); 
 const http = require('http');
-app = express();
-http.createServer(app);
+import {Server as WebSocketServer} from "socket.io"
 
-app.set('port',process.env.PORT||3000);// PORT Asignment
+
+const app = express();
+const httpServer =  http.createServer(app);    
+const  io = WebSocketServer(httpServer);
+
 //static files indication with dir path, and frontend directory 
-app.use(express.static(path.join(__dirname + '/public')));
+app.use(express.static(__dirname + '/public'));
 
 
 
 //start sever and listen in the port
-const server = app.listen(app.get('port'),()=>{
+app.set('port', process.env.PORT || 3000);// PORT Asignment
+
+app.listen(app.get('port'),()=>{
     console.log(`Server is listening on port ${app.get('port')}...` )
 })
-
-//websocket 
-const SocketIO = require('socket.io')
-const io = SocketIO(server); //listen in the server 
 
 io.on('connection',()=>{
     console.log('New socket conection');
