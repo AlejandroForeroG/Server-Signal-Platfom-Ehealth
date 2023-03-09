150747f95ce3e091056e33af60c5c2506c34b5d2
AlejandroForeroG <alejandro.forerog@gmail.com>
2023-03-09 23:15:07 +0000

    Main page with 3 measures addess, only working temperature for now

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const http = require('http');
 const WebSocketServer =  require("socket.io")
 
 
+
+
 const app = express();
 const Server =  http.createServer(app);    
 const  io = WebSocketServer(Server);
@@ -17,37 +19,51 @@ app.set('port', process.env.PORT || 3000);// PORT Asignment
 Server.listen(app.get('port'),()=>{
     console.log(`El servidor esta escuchando en el puerto ${app.get('port')}...` )
 })
-
+ 
 
 //event in io server 
 io.on('connection',(socket)=>{
+    
     console.log('\nNueva coneccion del socket: ', socket.handshake.address);
-
-    socket.on('disconnect',()=>{
-        console.log("\nComunicacion finalizada en: ", socket.handshake.address)
-    })
 
     var comprobante=0;
     //socket recieber for the rasberry data send 
     socket.on('rasberry:data', (data) => {
-        //buffer comprobation
-        if(comprobante!=1){
-            if(data.sampleGen!=1){
-                console.log("Esperando buffer...")
-            }else{
-                comprobante=1;
-                console.log(data);
-                io.emit('rasberry:data',(data));
-            }
-        }else{
-            console.log(data);
-            io.emit('rasberry:data',(data));
-        }
         
+        //buffer comprobation hacer promresa  🟩
+        // if(comprobante!=1){
+        //     if(data.sampleGen!=1){
+        //         console.log("Esperando buffer...")
+        //     }else{
+        //         comprobante=1;
+        //         console.log(data);
+                
+        //         io.emit('rasberry:data',(data));
+        //     }
+        // }else{
+        //     console.log(data);
+        //     io.emit('rasberry:data',(data));
+        // } 
+       
+        data.temperature=(comp(data.temperature,37,35));
+        io.emit('rasberry:data', data);
     });
+
 
     socket.on('error', (err) => {
         console.log(err.message)
     })
 
+      socket.on('disconnect',()=>{
+        console.log("\nComunicacion finalizada en: ", socket.handshake.address)
+    })
+
 });
+
+function comp(comp,max,min){
+    comp=comp;
+    if(comp>=max ||comp<=min ){
+        return comp
+    }else
+        return 0;
+}
--- public/app.js
+++ public/app.js
@@ -1,28 +1,97 @@
 const socket = io();
 let counter=0;
+let prober =0;
+let cont= 0;
+
 
 //when the rasberry send the data 
 socket.on('rasberry:data', (dataSerial) => {
-
     
-    Chart.data.labels.push(counter);
-    Chart.data.datasets.forEach(dataset => {
-        dataset.data.push(dataSerial.value)
-    });
+
+    // console.log(dataSerial)
+    // Chart.data.labels.push(counter);
+    // Chart.data.datasets.forEach(dataset => {
+    //     dataset.data.push(dataSerial)
+    // });
     //console.log(dataSerial)
-    counter++;
-    Chart.update();
+   
+    if(prober !=1){ // cambiar pro promesas 🟩
+        if(counter<10){
+            addData(Charte,dataSerial.sample,dataSerial.temperature)
+            Charte.update()
+            counter++;
+
+        }else{
+            prober =1
+            cont++
+        }
+    }else{
+        addData(Charte,dataSerial.sample,dataSerial.temperature)
+        Charte.update()
+        removeData(Charte)
+        cont++
+    }
+    
+    
+    
+    
+    console.log(dataSerial.sample)
+    // Chart.update();
 })
 
-const ctx = document.getElementById('myChart').getContext('2d');
-var Chart=new Chart(ctx, {
+
+const ctx = document.getElementById('myChartTemp').getContext('2d');
+let gradient = ctx.createLinearGradient(0,0,0,400);
+gradient.addColorStop(0,'rgba(58,123,213,1)')
+gradient.addColorStop(1,'rgba(0,210,255,0.3')
+
+Charte = new Chart(ctx, {
     type: 'line',
     data: {
-        labels: ['Serial Port'],
+        labels: ['Temperatura'],
         datasets: [{
-            label: 'serial',
-            backgrounColor:'rgb(52,73,74)',
-            borderColor:'rgb(41,128,185)',
+            label: 'Temperatura',
+            fill:true,
+            pointBackgrounColor:"rgb(189,195,199)",
+            borderColor:'rgb(189,195,199)',
+            backgrounColor:gradient,
+            data: [],
+        }],
+    },
+    options:{
+       animation: {
+            duration:"1000",
+    
+        }
+        }
+    
+});
+
+
+const ctxo = document.getElementById('myChartBMP');
+Charco = new Chart(ctxo, {
+    type: 'line',
+    data: {
+        labels: ['BMP'],
+        datasets: [{
+            label: 'BMP',
+            backgrounColor:'rgb(238, 54, 54)',
+            borderColor:'rgb(238, 54, 54)',
+            data: [],
+        }]
+    },
+    options:{}
+});
+
+const ctxb = document.getElementById('myChartox');
+Lalo = new Chart(ctxb, {
+    type: 'line',
+    data: {
+        labels: ['Saturacion de oxigeno'],
+        datasets: [{
+            label: 'Saturacion de oxigeno',
+            backgrounColor:'rgb(54, 82, 238)',
+            borderColor:'rgb(54, 82, 238)',
             data: [],
         }]
     },
@@ -32,3 +101,19 @@ var Chart=new Chart(ctx, {
 
 
 
+
+function addData(chart, label, data) {
+    chart.data.labels.push(label);
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data.push(data);
+    });
+    
+}
+
+function removeData(chart) {
+    chart.data.labels.shift();
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data.shift();
+    });
+    
+}
